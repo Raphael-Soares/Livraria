9debabd62d0eb0ca358ba209628b9eba8e20cde3
Raphael-Soares <soaresphaell@gmail.com>
2022-08-18 16:47:03 -0300

    Aula 6

--- livraria/settings.py
+++ livraria/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'core',
+    "rest_framework",
+    "core",
 ]
 
 MIDDLEWARE = [
