bcff600a4f2510a8ba99732853ebbd024828d1a1
Raphael-Soares <soaresphaell@gmail.com>
2022-08-18 16:51:53 -0300

    Aula 10

--- livraria/settings.py
+++ livraria/settings.py
@@ -1,4 +1,4 @@
-
+import os
 
 from pathlib import Path
 
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
@@ -117,3 +118,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
