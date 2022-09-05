06d179c6c874bab1ad4e08129765843dcef66479
Raphael-Soares <soaresphaell@gmai.com>
2022-09-05 14:31:06 -0300

    aula 11

--- core/models.py
+++ core/models.py
@@ -39,5 +39,7 @@ class Livro(models.Model):
         Editora, on_delete=models.PROTECT, related_name="livros"
     )
 
+    autores = models.ManyToManyField(Autor, related_name="livros")
+
     def __str__(self):
         return f"{self.titulo} ({self.quantidade}) - {self.preco}"
